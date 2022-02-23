de6ca9c1dd308c9451a2bd36eeedae05a4968c13
Büşra Nur Kuşçu <191118034@samsun.edu.tr>
2022-02-23 22:42:49 +0300

    int ve double veri tiplerinin minimum ve maksimum değer aralığında saklayabilecekleri sayıları gösteren örnek bir uygulama ve testi

--- TheLimitOfDataTypes/Program.cs
+++ TheLimitOfDataTypes/Program.cs
@@ -1,5 +1,9 @@
 ﻿sbyte number = 15;
+int numb = 0;
+double numbers = 1.2;
 
 Console.WriteLine($"{sbyte.MinValue} / {sbyte.MaxValue}");
 Console.WriteLine($"{byte.MinValue} / {byte.MaxValue}");
+Console.WriteLine($"{int.MinValue} / {int.MaxValue}");
+Console.WriteLine($"{double.MinValue} / {double.MaxValue}");
 Console.ReadKey();
--- TheLimitOfDataTypesTests/TheLimitOfDataTypesControlTest.cs
+++ TheLimitOfDataTypesTests/TheLimitOfDataTypesControlTest.cs
@@ -36,5 +36,35 @@ namespace TheLimitOfDataTypesTests
             Assert.Equal(maxValue, sbyte.MaxValue);
 
         }
+
+
+        [Fact]
+        public void int_data_type_control_tests()
+        {
+            // arrange
+            int minValue, maxValue;
+
+            //act
+            maxValue = 0;
+            minValue = -2147483648;
+
+            // assert
+            Assert.Equal(minValue, int.MinValue);
+
+        }
+        [Fact]
+        public void double_data_type_control_tests()
+        {
+            // arrange
+            double minValue, maxValue;
+
+            //act
+            maxValue = 1.123456789 + 308;
+
+            // assert
+            minValue = - 1.123456789 + 308;
+            
+        }
+
     }
 }
